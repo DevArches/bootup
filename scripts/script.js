64497627bc9a2b3267374ee723ef95a0cd6a3e93
Ryan Coulson <coulson.ryan@gmail.com>
2022-03-26 15:16:31 +0100

    Add more info
    Added Favicion and Lebenslauf

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,12 @@ function removeElement() {
   document.getElementById('moveable').remove();
   document.getElementById('abMe').disabled = false;
   document.getElementById('mySkills').disabled = false;
+  document.getElementById('myCV').disabled = false;
   document.getElementById('abMe').className = 'nes-btn';
   document.getElementById('mySkills').className = 'nes-btn';
+  document.getElementById('myCV').className = 'nes-btn';
 }
 const aboutText = `Hi, I'm Ryan. I'm currently learning App-Development in Hamburg Germany at College Berufliche Weiterbildung GmbH.<br><br> To finish my studies I need to complete a Praktikum starting 26.09.2022. <br><br> Under "My Skills" you can see what I will learn by the time my Praktikum starts.`;
-
-
-
 
 function aboutMe() {
   if (document.getElementById('moveable')) {
@@ -51,14 +50,15 @@ function mySkills() {
       <li>HTML</li>
       <li>CSS</li>
       <li>JavaScript</li>
-      <li>Java</li>
-      <li>PHP</li>
       <li>Node.js</li>
       <li>Express</li>
       <li>MongoDB</li>
       <li>Linux</li>
       <li>GitHub</li>
+      <li>Java*</li>
+      <li>PHP*</li>
     </ul>
+    <p>* not yet completed in Umschulung</p>
   </div>`;
     mySkills.appendChild(mySkillsTitle);
     mySkills.appendChild(mySkillsDiv);
@@ -69,3 +69,27 @@ function mySkills() {
     document.getElementById('mySkills').className = 'nes-btn is-disabled';
   }
 }
+
+function myCV() {
+  if (document.getElementById('moveable')) {
+    removeElement();
+    myCV();
+  } else {
+    let myCV = document.createElement('div');
+    myCV.className = 'nes-container with-title nes-pointer';
+    myCV.id = 'moveable';
+    let myCVTitle = document.createElement('span');
+    myCVTitle.className = 'title';
+    myCVTitle.style = 'border: 3px solid black;';
+    myCVTitle.innerHTML = `My CV <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
+    let myCVP = document.createElement('p');
+    myCVP.innerHTML = `<embed src="RyanCoulsonLebenslauf.pdf" height="750px" width="500px"/>`
+    myCV.appendChild(myCVTitle);
+    myCV.appendChild(myCVP);
+    document.body.appendChild(myCV);
+    drag();
+    document.getElementById('myCV').disabled = true;
+    document.getElementById('myCV').className = 'nes-btn is-disabled';
+  }
+}
+
