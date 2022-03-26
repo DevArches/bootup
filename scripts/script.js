adb05a5317949c548f6b6cb1483dc13df94f1f6a
Ryan Coulson <coulson.ryan@gmail.com>
2022-03-26 16:01:38 +0100

    Added under construction

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,13 @@ function removeElement() {
   document.getElementById('abMe').disabled = false;
   document.getElementById('mySkills').disabled = false;
   document.getElementById('myCV').disabled = false;
+  document.getElementById('construct').disabled = false;
   document.getElementById('abMe').className = 'nes-btn';
   document.getElementById('mySkills').className = 'nes-btn';
   document.getElementById('myCV').className = 'nes-btn';
+  document.getElementById('construct').className = 'nes-btn';
 }
-const aboutText = `Hi, I'm Ryan. I'm currently learning App-Development in Hamburg Germany at College Berufliche Weiterbildung GmbH.<br><br> To finish my studies I need to complete a Praktikum starting 26.09.2022. <br><br> Under "My Skills" you can see what I will learn by the time my Praktikum starts.`;
+const aboutText = `Hallo, ich bin Ryan. Ich lerne derzeit Anwendungsentwicklung in Hamburg Deutschland bei der College Berufliche Weiterbildung GmbH.<br><br> Um meine Umschulung abzuschließen, muss ich vom 26.09.2022 bis 21.11.2023 ein Praktikum absolvieren. <br><br> Unter „Meine Skills“ kannst du sehen was ich bis zum Beginn meines Praktikums lernen werde.`;
 
 function aboutMe() {
   if (document.getElementById('moveable')) {
@@ -20,7 +22,7 @@ function aboutMe() {
     let aboutMeTitle = document.createElement('span');
     aboutMeTitle.className = 'title';
     aboutMeTitle.style = 'border: 3px solid black;';
-    aboutMeTitle.innerHTML = `About Me <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
+    aboutMeTitle.innerHTML = `Über Mich <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
     let aboutMeP = document.createElement('p');
     aboutMeP.innerHTML = aboutText;
     aboutMe.appendChild(aboutMeTitle);
@@ -42,7 +44,7 @@ function mySkills() {
     let mySkillsTitle = document.createElement('span');
     mySkillsTitle.className = 'title';
     mySkillsTitle.style = 'border: 3px solid black;';
-    mySkillsTitle.innerHTML = `My Skills <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
+    mySkillsTitle.innerHTML = `Meine Skills <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
     let mySkillsDiv = document.createElement('div');
     mySkillsDiv.className = 'lists';
     mySkillsDiv.innerHTML = `<div class="lists">
@@ -81,8 +83,8 @@ function myCV() {
     let myCVTitle = document.createElement('span');
     myCVTitle.className = 'title';
     myCVTitle.style = 'border: 3px solid black;';
-    myCVTitle.innerHTML = `My CV <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
-    let myCVP = document.createElement('p');
+    myCVTitle.innerHTML = `Mein Lebenslauf <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
+    let myCVP = document.createElement('div');
     myCVP.innerHTML = `<embed src="RyanCoulsonLebenslauf.pdf" height="750px" width="500px"/>`
     myCV.appendChild(myCVTitle);
     myCV.appendChild(myCVP);
@@ -93,3 +95,25 @@ function myCV() {
   }
 }
 
+function construction() {
+  if (document.getElementById('moveable')) {
+    removeElement();
+    construction();
+  } else {
+    let construction = document.createElement('div');
+    construction.className = 'nes-container with-title nes-pointer';
+    construction.id = 'moveable';
+    let constructionTitle = document.createElement('span');
+    constructionTitle.className = 'title';
+    constructionTitle.style = 'border: 3px solid black;';
+    constructionTitle.innerHTML = `Aufbau <button type='button' onclick='removeElement()'><i class="nes-icon close is-small"></i></button>`;
+    let constructionP = document.createElement('p');
+    constructionP.innerHTML = `Diese Website befindet sich derzeit im Aufbau.<br><br> ToDo:<br> <ul><li>Responsive Design</li><li>Projektbereich</li><li>Animierter Hintergrund</li></ul>`;
+    construction.appendChild(constructionTitle);
+    construction.appendChild(constructionP);
+    document.body.appendChild(construction);
+    drag();
+    document.getElementById('construct').disabled = true;
+    document.getElementById('construct').className = 'nes-btn is-disabled';
+  }
+}
